24b78595bcaa12f9469808cc0a46c65677cfb2e1
sidhujag <sidhujag@hotmail.com>
2016-10-26 18:18:52 -0700

    password check

--- src/alias.cpp
+++ src/alias.cpp
@@ -1701,7 +1701,8 @@ UniValue aliasnew(const UniValue& params, bool fHelp) {
 	if(!defaultKey.IsFullyValid())
 		throw runtime_error("SYSCOIN_ALIAS_RPC_ERROR: ERRCODE: 5504 - " + _("Generated public key not fully valid"));
 	pwalletMain->AddKeyPubKey(key, defaultKey);			
-
+	if(!HaveKey(defaultKey.GetID())
+		throw runtime_error("SYSCOIN_ALIAS_RPC_ERROR: ERRCODE: 5504 - " + _("Choose a different password"));
 	CScript scriptPubKeyOrig;
 	CMultiSigAliasInfo multiSigInfo;
 	if(aliasNames.size() > 0)
