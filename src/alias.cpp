85c8565836e24f32ef881ec022f627b255a05526
sidhujag <sidhujag@hotmail.com>
2016-10-29 22:55:03 -0700

    TYPO

--- src/alias.cpp
+++ src/alias.cpp
@@ -1123,7 +1123,7 @@ bool CheckAliasInputs(const CTransaction &tx, int op, int nOut, const vector<vec
 		else if(op == OP_ALIAS_PAYMENT)
 		{
 			// if alias is expired, return silently, dont add payments to db
-			if(vtxPos.empty())
+			if(isExpired)
 				return true;
 			const uint256 &txHash = tx.GetHash();
 			vector<uint256> vtxPaymentPos;
