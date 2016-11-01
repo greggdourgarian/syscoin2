9c0bca5ee616d97b3c55334b7fa28e8d295e11e7
sidhujag <sidhujag@hotmail.com>
2016-11-01 15:41:58 -0700

    typo

--- src/alias.cpp
+++ src/alias.cpp
@@ -1707,7 +1707,8 @@ void TransferAliasBalances(const vector<unsigned char> &vchAlias, const CScript&
     }
 	if(nAmount > 0)
 	{
-		CScript scriptChangeOrig << CScript::EncodeOP_N(OP_ALIAS_PAYMENT) << vchAlias << OP_2DROP;
+		CScript scriptChangeOrig;
+		scriptChangeOrig << CScript::EncodeOP_N(OP_ALIAS_PAYMENT) << vchAlias << OP_2DROP;
 		scriptChangeOrig += scriptPubKeyTo;
 		CRecipient recipient = {scriptChangeOrig, nAmount, false};
 		vecSend.push_back(recipient);
