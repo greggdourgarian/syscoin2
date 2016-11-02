9fd148da179085074ea7814c061556547890ce92
sidhujag <sidhujag@hotmail.com>
2016-11-01 17:08:35 -0700

    remove hceck

--- src/alias.cpp
+++ src/alias.cpp
@@ -1707,7 +1707,7 @@ void TransferAliasBalances(const vector<unsigned char> &vchAlias, const CScript&
     }
 	if(nAmount > 0)
 	{
-		CRecipient recipient = {scriptPubKeyTo, nAmount, true};
+		CRecipient recipient = {scriptPubKeyTo, nAmount, false};
 		vecSend.push_back(recipient);
 	}
 }
