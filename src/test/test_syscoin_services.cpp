a1e9f4e7d35ee7bf4574aca10b698063e33b4c38
sidhujag <sidhujag@hotmail.com>
2016-11-03 13:32:43 -0700

    wip escrow test

--- src/test/test_syscoin_services.cpp
+++ src/test/test_syscoin_services.cpp
@@ -1373,8 +1373,8 @@ void EscrowClaimRelease(const string& node, const string& guid)
 	UniValue resArray = r.get_array();
 	string strRawTx = resArray[0].get_str();
 	BOOST_CHECK_NO_THROW(r = CallRPC(node, "escrowcompleterelease " + guid + " " + strRawTx));
-	GenerateBlocks(10, node);
-
+	GenerateBlocks(5, node);
+	GenerateBlocks(5, node);
 	BOOST_CHECK_NO_THROW(r = CallRPC(node, "offerinfo " + offer));
 	int nQtyOfferAfter = atoi(find_value(r.get_obj(), "quantity").get_str().c_str());
 	// release deducts qty
