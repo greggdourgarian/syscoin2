9e447e443dca73b623eda9868d22ea7d84192def
sidhujag <sidhujag@hotmail.com>
2016-11-08 10:15:44 -0800

    fix test

--- src/test/syscoin_offer_tests.cpp
+++ src/test/syscoin_offer_tests.cpp
@@ -377,9 +377,11 @@ BOOST_AUTO_TEST_CASE (generate_offeraccept)
 	BOOST_CHECK_THROW(r = CallRPC("node2", "offeraccept buyeralias3 " + offerguid + " 0 message"), runtime_error);
 
 	// perform an accept on more items than available, qty doesnt update so its valid
-	acceptguid = OfferAccept("node1", "node2", "buyeralias3", offerguid, "100", "message");
+	BOOST_CHECK_NO_THROW(r = CallRPC("node2", "offeraccept buyeralias3 " + offerguid + " 100 message"));
+	const UniValue &arr = r.get_array();
+	acceptguid = arr[1].get_str();
 	// payment ack won't work as it tries to adjust qty
-	BOOST_CHECK_THROW(CallRPC("node1", "offeracceptacknowledge " + offerguid + " " + acceptguid), runtime_error);
+	BOOST_CHECK_THROW(r = CallRPC("node1", "offeracceptacknowledge " + offerguid + " " + acceptguid), runtime_error);
 }
 BOOST_AUTO_TEST_CASE (generate_linkedaccept)
 {
