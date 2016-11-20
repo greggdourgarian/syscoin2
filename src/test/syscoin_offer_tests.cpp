b08b84d7283b33003a24392bb60ef446e56aa133
sidhujag <sidhujag@hotmail.com>
2016-11-20 08:43:52 -0800

    fix tests

--- src/test/syscoin_offer_tests.cpp
+++ src/test/syscoin_offer_tests.cpp
@@ -397,7 +397,7 @@ BOOST_AUTO_TEST_CASE (generate_linkedaccept)
 
 	string offerguid = OfferNew("node1", "node1aliaslinked", "category", "title", "10", "0.05", "description", "USD", "nocert");
 	OfferAddWhitelist("node1", offerguid, "node2aliaslinked", "0");
-	string lofferguid = OfferLink("node2", "node2aliaslinked", offerguid, "0", "newdescription");
+	string lofferguid = OfferLink("node2", "node2aliaslinked", offerguid, "5", "newdescription");
 
 	LinkOfferAccept("node1", "node3", "node3aliaslinked", lofferguid, "6", "message", "node2");
 }
