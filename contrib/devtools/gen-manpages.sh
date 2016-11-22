9a0537c06310061723aa86f83aa7dadbbdf0dfa5
Willy <k.o.willy@gmail.com>
2016-11-21 17:22:15 -0800

    updated man files

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -12,7 +12,8 @@ SYSCOINQT=${SYSCOINQT:-$SRCDIR/qt/syscoin-qt}
 [ ! -x $SYSCOIND ] && echo "$SYSCOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-SYSVER=($($SYSCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+SYSVER=$($SYSCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6 }')
+SYSCOMID=$($SYSCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $7 }')
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for syscoind if --version-string is not set,
@@ -22,8 +23,8 @@ $SYSCOIND --version | sed -n '1!p' >> footer.h2m
 
 for cmd in $SYSCOIND $SYSCOINCLI $SYSCOINTX $SYSCOINQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${SYSVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${SYSVER[1]}//g" ${MANDIR}/${cmdname}.1
+  $(help2man -N --version-string=$SYSVER --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd})
+  sed -i "s/\\\-${SYSCOMID}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
