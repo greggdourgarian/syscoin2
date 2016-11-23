0dba1d4a74e03d395b36166be3cbce199427c680
sidhujag <sidhujag@hotmail.com>
2016-11-22 22:10:23 -0800

    max message to 4kb and max data to 75* opreturn

--- src/message.h
+++ src/message.h
@@ -24,7 +24,7 @@ CScript RemoveMessageScriptPrefix(const CScript& scriptIn);
 extern bool IsSys21Fork(const uint64_t& nHeight);
 void MessageTxToJSON(const int op, const std::vector<unsigned char> &vchData, const std::vector<unsigned char> &vchHash, UniValue &entry);
 std::string messageFromOp(int op);
-static const unsigned int MAX_MESSAGE_LENGTH = 3000;
+static const unsigned int MAX_MESSAGE_LENGTH = 1028*4;
 static const unsigned int MAX_ENCRYPTED_MESSAGE_LENGTH = MAX_MESSAGE_LENGTH + 85;
 
 class CMessage {
