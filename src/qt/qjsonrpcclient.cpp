e8c16fa21d52438672ec92ceabb22824bc7d1f1f
sidhujag <sidhujag@hotmail.com>
2016-11-29 15:47:43 -0800

    handle rpc authentication

--- src/qt/qjsonrpcclient.cpp
+++ src/qt/qjsonrpcclient.cpp
@@ -30,7 +30,9 @@ void RpcClient::setPassword(const QString &password) {
     m_password = password;
 }
 void RpcClient::sendRequest(QNetworkAccessManager *nam, const QString &method, const QString &param) {
-
+	
+	connect(nam, SIGNAL(authenticationRequired(QNetworkReply*, QAuthenticator*)), this, SLOT(handleAuthenticationRequired(QNetworkReply*, QAuthenticator*)));
+	
 	QString data = "{\"jsonrpc\": \"1.0\", \"id\":\"syscoinRpcClient\", ""\"method\": \"" + method + "\", \"params\": [" + param + "] }";
 	QJsonDocument doc = QJsonDocument::fromJson(data.toUtf8());
 	QByteArray postData = doc.toJson();
