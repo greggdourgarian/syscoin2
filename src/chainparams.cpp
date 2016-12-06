474f88a08791cbe91e14b545e37b595ea59fb519
sidhujag <sidhujag@hotmail.com>
2016-12-06 14:21:32 -0800

    fix chainparam

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -254,7 +254,7 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
 
-      /*  base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
 
@@ -265,26 +265,11 @@ public:
 
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
-*/
-		// SYSCOIN/BITCOIN key prefixes
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
-        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1,128);
 
-        
-        base58Prefixes[PUBKEY_ADDRESS_ZEC] = {0x1C,0xB8};
-		base58Prefixes[SCRIPT_ADDRESS_ZEC] = {0x1C,0xBD};
-
-		base58Prefixes[PUBKEY_ADDRESS_SYS] = std::vector<unsigned char>(1,63);
-
-		base58Prefixes[SECRET_KEY_SYS] = std::vector<unsigned char>(1,191);
-        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
-        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
         fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
-		// change this back to false
-        fRequireStandard = true;
+        fRequireStandard = false;
         fMineBlocksOnDemand = false;
         fTestnetToBeDeprecatedFieldRPC = true;
 
