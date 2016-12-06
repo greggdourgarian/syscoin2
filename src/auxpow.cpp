316eda58ce6169bb92621956ae42f590adbc52f3
sidhujag <sidhujag@hotmail.com>
2016-12-06 11:11:27 -0800

    syncs with 2.0

--- src/auxpow.cpp
+++ src/auxpow.cpp
@@ -75,7 +75,7 @@ CAuxPow::check (const uint256& hashAuxBlock, int nChainId,
     if (nIndex != 0)
         return error("AuxPow is not a generate");
 
-    if (parentBlock.nVersion.GetChainId () == nChainId)
+    if (parentBlock.nVersion.GetChainId () == nChainId && chainActive.Tip()->nHeight >= SYSCOIN_FORK1)
         return error("Aux POW parent has our chain ID");
 
     if (vChainMerkleBranch.size() > 30)
