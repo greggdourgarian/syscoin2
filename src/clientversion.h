e72b730e81380e655eaa4b55f2f3a656524c3fa1
sidhujag <sidhujag@hotmail.com>
2016-12-08 08:32:10 -0800

    RC7

--- src/clientversion.h
+++ src/clientversion.h
@@ -51,7 +51,7 @@
 #include <string>
 #include <vector>
 // SYSCOIN
-static const std::string SYSCOIN_CLIENT_VERSION = "2.10RC6";
+static const std::string SYSCOIN_CLIENT_VERSION = "2.10RC7";
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   10000 * CLIENT_VERSION_MINOR
