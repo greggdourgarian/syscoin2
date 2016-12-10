9d31fc64b2213e8b76e5d5b00eaedd41c7eabb6f
sidhujag <sidhujag@hotmail.com>
2016-12-09 19:50:06 -0800

    compile

--- src/offer.cpp
+++ src/offer.cpp
@@ -2493,7 +2493,7 @@ UniValue offerupdate(const UniValue& params, bool fHelp) {
 			vecSend.push_back(aliasRecipientLink);
 	}
 
-	SendMoneySyscoin(vecSend, recipient.nAmount+aliasRecipient.nAmount+fee.nAmount, false, wtx, wtxAliasIn, outPoint.n, alias.multiSigInfo.vchAliases.size() > 0, true, NULL, wtxLinkAliasIn, outPointLink.n);
+	SendMoneySyscoin(vecSend, recipient.nAmount+aliasRecipient.nAmount+fee.nAmount, false, wtx, wtxAliasIn, outPoint.n, alias.multiSigInfo.vchAliases.size() > 0, NULL, wtxLinkAliasIn, outPointLink.n);
 	UniValue res(UniValue::VARR);
 	if(alias.multiSigInfo.vchAliases.size() > 0)
 	{
