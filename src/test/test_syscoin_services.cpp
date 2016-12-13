12217961337447699a99466477a910b4452e03d8
sidhujag <sidhujag@hotmail.com>
2016-12-13 11:00:31 -0800

    fix test

--- src/test/test_syscoin_services.cpp
+++ src/test/test_syscoin_services.cpp
@@ -297,7 +297,7 @@ void ExpireAlias(const string& alias)
 		BOOST_CHECK_EQUAL(find_value(r.get_obj(), "expired").get_int(), 0);	
 		int64_t expiryTime = find_value(r.get_obj(), "expires_on").get_int64();
 		string cmd = strprintf("setmocktime %lld", expiryTime);
-		BOOST_CHECK_NO_THROW(r = CallRPC("node1", cmd));
+		BOOST_CHECK_NO_THROW(CallRPC("node1", cmd));
 	}
 	r = CallRPC("node2", "aliasinfo " + alias);
 	if(r.isObject())
@@ -305,7 +305,7 @@ void ExpireAlias(const string& alias)
 		BOOST_CHECK_EQUAL(find_value(r.get_obj(), "expired").get_int(), 0);	
 		int64_t expiryTime = find_value(r.get_obj(), "expires_on").get_int64();
 		string cmd = strprintf("setmocktime %lld", expiryTime);
-		BOOST_CHECK_NO_THROW(r = CallRPC("node2", cmd));
+		BOOST_CHECK_NO_THROW(CallRPC("node2", cmd));
 	}
 	r = CallRPC("node3", "aliasinfo " + alias);
 	if(r.isObject())
@@ -313,7 +313,7 @@ void ExpireAlias(const string& alias)
 		BOOST_CHECK_EQUAL(find_value(r.get_obj(), "expired").get_int(), 0);	
 		int64_t expiryTime = find_value(r.get_obj(), "expires_on").get_int64();
 		string cmd = strprintf("setmocktime %lld", expiryTime);
-		BOOST_CHECK_NO_THROW(r = CallRPC("node3", cmd));
+		BOOST_CHECK_NO_THROW(CallRPC("node3", cmd));
 	}
 	GenerateBlocks(5);
 	// ensure alias is expired
