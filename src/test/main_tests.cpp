15c32d59a1c3afcb1f3440ef44eac0f7e104f019
sidhujag <sidhujag@hotmail.com>
2016-12-14 19:55:57 -0800

    fix test

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -54,7 +54,7 @@ BOOST_AUTO_TEST_CASE(subsidy_limit_test)
         nSum += nSubsidy;
     }
 	BOOST_CHECK(MoneyRange(nSum));
-    BOOST_CHECK_EQUAL(nSum, 79993363628897596ULL);
+    BOOST_CHECK_EQUAL(nSum, 79993364266897596ULL);
 }
 
 bool ReturnFalse() { return false; }
