1abaa3603216891d3be2edca982674f272345e4a
sidhujag <sidhujag@hotmail.com>
2016-12-17 18:52:00 -0800

    remove +1

--- src/test/syscoin_alias_tests.cpp
+++ src/test/syscoin_alias_tests.cpp
@@ -190,8 +190,8 @@ BOOST_AUTO_TEST_CASE (generate_offer_aliasexpiry_resync)
 	BOOST_CHECK(aliasoldexpiry <= mediantime);
 	BOOST_CHECK(aliasnewexpiry > mediantime);
 
-	//StopNode("node1");
-	//StartNode("node1");
+	StopNode("node1");
+	StartNode("node1");
 
 	// aliasnew should still be active, but offer was set to aliasold so it should be expired
 	ExpireAlias("aliasold");
