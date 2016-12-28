33933775922fdb873214a68587329a7fb56c0541
sidhujag <sidhujag@hotmail.com>
2016-12-27 22:31:07 -0800

    compile

--- src/auxpow.cpp
+++ src/auxpow.cpp
@@ -96,7 +96,7 @@ CAuxPow::check (const uint256& hashAuxBlock, int nChainId,
     if (nIndex != 0)
         return error("AuxPow is not a generate");
 
-    if (parentBlock.nVersion.GetChainId () == nChainId)
+    if (parentBlock.GetChainId () == nChainId)
         return error("Aux POW parent has our chain ID");
 
     if (vChainMerkleBranch.size() > 30)
