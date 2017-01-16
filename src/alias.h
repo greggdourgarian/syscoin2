facd00f15936303373a042f45ce6c17b87fc890c
sidhujag <sidhujag@hotmail.com>
2017-01-16 14:56:13 -0800

    typo

--- src/alias.h
+++ src/alias.h
@@ -5,7 +5,7 @@
 #include "dbwrapper.h"
 #include "script/script.h"
 #include "serialize.h"
-#include "chainparams.h"
+#include "consensus/params.h"
 #include "sync.h" 
 class CWalletTx;
 class CTransaction;
