343b1c78b740cc5bdac93a1fef6c68e9a307db80
sidhujag <sidhujag@hotmail.com>
2017-02-01 13:02:17 -0800

    fix ban alias calls

--- src/test/test_syscoin_services.cpp
+++ src/test/test_syscoin_services.cpp
@@ -350,21 +350,38 @@ void CreateSysRatesIfNotExist()
 	string data = "{\\\"rates\\\":[{\\\"currency\\\":\\\"USD\\\",\\\"rate\\\":2690.1,\\\"precision\\\":2},{\\\"currency\\\":\\\"EUR\\\",\\\"rate\\\":2695.2,\\\"precision\\\":2},{\\\"currency\\\":\\\"GBP\\\",\\\"rate\\\":2697.3,\\\"precision\\\":2},{\\\"currency\\\":\\\"CAD\\\",\\\"rate\\\":2698.0,\\\"precision\\\":2},{\\\"currency\\\":\\\"BTC\\\",\\\"rate\\\":100000.0,\\\"fee\\\":75,\\\"escrowfee\\\":0.01,\\\"precision\\\":8},{\\\"currency\\\":\\\"ZEC\\\",\\\"rate\\\":1000000.0,\\\"fee\\\":50,\\\"escrowfee\\\":0.01,\\\"precision\\\":8},{\\\"currency\\\":\\\"SYS\\\",\\\"rate\\\":1.0,\\\"fee\\\":1000,\\\"escrowfee\\\":0.005,\\\"precision\\\":2}]}";
 	// should get runtime error if doesnt exist
 	try{
-		CallRPC("node1", "aliasupdate sysrates.peg sysrates.peg " + data);
+		UniValue r = CallRPC("node1", "aliasinfo sysrates.peg");
+		if(r.isObject())
+			AliasUpdate("node1", "sysrates.peg", data, "priv");
+		else
+			AliasNew("node1", "sysrates.peg", "password", data);
 	}
 	catch(const runtime_error& err)
 	{
 		GenerateBlocks(200, "node1");	
 		GenerateBlocks(200, "node2");	
 		GenerateBlocks(200, "node3");
-		BOOST_CHECK_NO_THROW(CallRPC("node1", "aliasnew sysrates.peg sysrates.peg password " + data));
+		try
+		{
+			AliasNew("node1", "sysrates.peg", "password", data);
+		}
+		catch(const runtime_error &e)
+		{
+			throw runtime_error(e.what());
+		}
 	}
-	GenerateBlocks(5, "node1");
 }
 void CreateSysBanIfNotExist()
 {
 	string data = "{}";
-	BOOST_CHECK_NO_THROW(CallRPC("node1", "aliasnew sysrates.peg sysban password " + data));
+	try
+	{
+		AliasNew("node1", "sysban", "password", data);
+	}
+	catch(const runtime_error &e)
+	{
+		throw runtime_error(e.what());
+	}
  	GenerateBlocks(5, "node1");
 }
 void CreateSysCategoryIfNotExist()
@@ -383,19 +400,52 @@ void CreateSysCategoryIfNotExist()
 void AliasBan(const string& node, const string& alias, int severity)
 {
 	string data = "{\\\"aliases\\\":[{\\\"id\\\":\\\"" + alias + "\\\",\\\"severity\\\":" + boost::lexical_cast<string>(severity) + "}]}";
-	CallRPC(node, "aliasupdate sysrates.peg sysban " + data);
+	string strPasswordHex =  "\"\"";
+	string strPrivateHex = "\"\"";
+	string strEncryptionPrivateKeyHex = "\"\"";
+	string acceptTransfers = "\"\"";
+	string safesearch = "\"\"";
+	string expires = "\"\"";
+	string nrequired = "\"\"";
+	string aliases = "\"\"";
+	string pubkey = "\"\"";
+	string salt = "\"\"";
+	string aliasname = "sysban";
+	CallRPC(node, "aliasupdate sysrates.peg " + aliasname + " " + pubdata + " " + strPrivateHex + " " + safesearch + " " + pubkey + " " + strPasswordHex + " " + acceptTransfers + " " + expires + " " + nrequired + " " + aliases + " " + salt + " " + strEncryptionPrivateKeyHex);
 	GenerateBlocks(5, node);
 }
 void OfferBan(const string& node, const string& offer, int severity)
 {
 	string data = "{\\\"offers\\\":[{\\\"id\\\":\\\"" + offer + "\\\",\\\"severity\\\":" + boost::lexical_cast<string>(severity) + "}]}";
-	CallRPC(node, "aliasupdate sysrates.peg sysban " + data);
+	string strPasswordHex =  "\"\"";
+	string strPrivateHex = "\"\"";
+	string strEncryptionPrivateKeyHex = "\"\"";
+	string acceptTransfers = "\"\"";
+	string safesearch = "\"\"";
+	string expires = "\"\"";
+	string nrequired = "\"\"";
+	string aliases = "\"\"";
+	string pubkey = "\"\"";
+	string salt = "\"\"";
+	string aliasname = "sysban";
+	CallRPC(node, "aliasupdate sysrates.peg " + aliasname + " " + pubdata + " " + strPrivateHex + " " + safesearch + " " + pubkey + " " + strPasswordHex + " " + acceptTransfers + " " + expires + " " + nrequired + " " + aliases + " " + salt + " " + strEncryptionPrivateKeyHex);
  	GenerateBlocks(5, node);
 }
 void CertBan(const string& node, const string& cert, int severity)
 {
-	string data = "{\\\"certs\\\":[{\\\"id\\\":\\\"" + cert + "\\\",\\\"severity\\\":" + boost::lexical_cast<string>(severity) + "}]}";
-	CallRPC(node, "aliasupdate sysrates.peg sysban " + data);
+	string pubdata = "{\\\"certs\\\":[{\\\"id\\\":\\\"" + cert + "\\\",\\\"severity\\\":" + boost::lexical_cast<string>(severity) + "}]}";
+	string strPasswordHex =  "\"\"";
+	string strPrivateHex = "\"\"";
+	string strEncryptionPrivateKeyHex = "\"\"";
+	string acceptTransfers = "\"\"";
+	string safesearch = "\"\"";
+	string expires = "\"\"";
+	string nrequired = "\"\"";
+	string aliases = "\"\"";
+	string pubkey = "\"\"";
+	string salt = "\"\"";
+	string aliasname = "sysban";
+	CallRPC(node, "aliasupdate sysrates.peg " + aliasname + " " + pubdata + " " + strPrivateHex + " " + safesearch + " " + pubkey + " " + strPasswordHex + " " + acceptTransfers + " " + expires + " " + nrequired + " " + aliases + " " + salt + " " + strEncryptionPrivateKeyHex);
  	GenerateBlocks(5, node);
 }
 void ExpireAlias(const string& alias)
