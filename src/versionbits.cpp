cfff30e955b7d5056ced93f3ca47fe21d86669c4
sidhujag <sidhujag@hotmail.com>
2017-02-03 21:35:16 -0800

    typo

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -122,7 +122,7 @@ protected:
 
     bool Condition(const CBlockIndex* pindex, const Consensus::Params& params) const
     {
-        return (((pindex->nVersion.GetBaseVersion() & VERSIONBITS_TOP_MASK) == VERSIONBITS_TOP_BITS) && (pindex->nVersion.GetBaseVersion() & Mask(params)) != 0);
+        return (((pindex->GetBaseVersion() & VERSIONBITS_TOP_MASK) == VERSIONBITS_TOP_BITS) && (pindex->nVersion.GetBaseVersion() & Mask(params)) != 0);
     }
 
 public:
