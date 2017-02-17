eea5f0a0eacb139dede3dd8aba3a882230d87134
sidhujag <sidhujag@hotmail.com>
2017-02-16 22:47:50 -0800

    typo

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2487,8 +2487,7 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
                 unsigned int nBytes = GetVirtualTransactionSize(txNew);
 
                 // Remove scriptSigs if we used dummy signatures for fee calculation
-				// SYSCOIN
-                if (!sign && !sysTx) {
+                if (!sign) {
                     for (auto& vin : txNew.vin)
                         vin.scriptSig = CScript();
                     txNew.wit.SetNull();
